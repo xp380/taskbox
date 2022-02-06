3386202714e5c591189369b9aceb3a4a093bd40c
Vincent Kouoï <vincentkouoi@gmail.com>
2022-02-06 17:42:52 +0100

    taskbox UI

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //👇 Location of our stories
+  stories: ["../src/components/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+};
